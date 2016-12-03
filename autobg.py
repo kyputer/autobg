348f7c560fca5f73c13e64ca91c11e834133f58a
root <root@rzrbs.localdomain>
2016-12-03 16:21:48 -0500

    about to parse resolution

--- autobg.py
+++ autobg.py
@@ -7,6 +7,8 @@ RES_Y = 768
 #IMG_DUR = 1200
 #SEED_IMAGES = 5
 
+import subprocess
+import PIL
 from PIL import Image
 from sys import stdout
 from sys import exit
@@ -16,8 +18,12 @@ def find_resolution():
     res_x = 0
     res_y = 0
 
-    p1 = subprocess.Popen(["xrandr"], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["grep", regex_search], stdin=p1.stdout, stdout=subprocess.PIPE)
-    p1.stdout.close()
-    output = p2.communicate()[0]
+    res = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
 
+    res_x = RES_X
+    res_y = RES_Y
+    
+    print (output)
+
+    return int(res_x), int(res_y)
+find_resolution()
