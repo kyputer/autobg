9cb308b3bea8507c266dee4919219ed492adebad
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-01-02 13:36:15 -0500

    Use python-crontab

--- autobg.py
+++ autobg.py
@@ -16,14 +16,14 @@ import time
 import requests
 import random
 import json
-#import PIL
-#from PIL import Image
-#import schedule
+from crontab import CronTab
 
 parser = argparse.ArgumentParser(description='New daily backgrounds from flickr.')
 parser.add_argument('-k', '--keyword', help='Set keyword')
 parser.add_argument('-d', '--download-bg', help='Download new image from flickr', action="store_true")
 parser.add_argument('-c','--change-bg', help='Change Background to Downloaded Image', action="store_true")
+parser.add_argument('-s','--stop-job', help='Stop periodicaly downloading new images', action="store_true")
+parser.add_argument('-i','--interval', type=int, help='Interval for change, in minutes')
 
 def load_configs():
     """ Retrieve configs from .autobg file """
@@ -83,28 +83,48 @@ def handle_bg_change(filename, filepath):
     subprocess.call(["gsettings", "set", "org.gnome.desktop.background",
                     "picture-uri", "file://" + os.path.join(filepath, filename)])
 
-def a_dayold(s_time):
+def a_interval_old(s_time):
     """ Check if time is a day hold"""
+    global INTERVAL
     tm = time.strptime(s_time, "%m.%d.%y-%H:%M")
-    if (time.mktime(time.localtime()) - time.mktime(tm)) >= 86400000:
+    if (time.mktime(time.localtime()) - time.mktime(tm)) >= (INTERVAL * 60):
         return True
     return False
 
 def change_bg_if_old():
     """ Change background on first run or after a day """
     global CONFIGS
-    if not(CONFIGS) or a_dayold(CONFIGS["updated_at"]):
+    if not(CONFIGS) or a_interval_old(CONFIGS["updated_at"]):
         download_new_image()
         change_bg()
-    #schedule_next run for when a day old
+
+def schedule_next_download(disable=False):
+    global CONFIGS, KEYWORD,INTERVAL
+    configs = CONFIGS
+    cron = CronTab(user=True)
+    if 'job_cmd' in configs.keys():
+        cron.remove(cron.find_command((configs['job_cmd'])))
+    if not disable:
+        configs['job_cmd'] = job_cmd = "%s -k %s -i %i" % (os.path.realpath(__file__), KEYWORD, INTERVAL)
+        job = cron.new(command=job_cmd)
+        day = (INTERVAL%10080)//1440
+        hour = ((INTERVAL%10080)%1440)//60
+        minute = ((INTERVAL%10080)%1440)%60
+        job.day.every(1 or day)
+        job.hour.every(1 or hour)
+        job.minute.every(1 or minute)
+        job.enable()
+        set_configs(configs)
+    cron.write()
 
 if __name__ == '__main__':
-    global CONFIGS, KEYWORD
+    global CONFIGS, KEYWORD, INTERVAL
     TMP_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
     #print(TMP_PATH)
     CONFIGS = load_configs()
     args = parser.parse_args()
     KEYWORD = args.keyword or "nature"
+    INTERVAL = args.interval or 1440
     if not(args.download_bg or args.change_bg):
         change_bg_if_old()
     else:
@@ -113,3 +133,4 @@ if __name__ == '__main__':
             change_bg()
         if args.change_bg:
             change_bg()
+    schedule_next_download(args.stop_job)
